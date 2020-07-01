aff79ce41bd701e85f22310071476f274c0526ef
quinnc <qcollins6@cps.edu>
2020-06-30 23:51:49 -0500

    Address NITS and adapt to updated main branch

--- portfolio/src/main/java/com/google/sps/data/Video.java
+++ portfolio/src/main/java/com/google/sps/data/Video.java
@@ -1,4 +1,9 @@
 package com.google.sps.data;
+import com.google.sps.data.PropertyNames;
+import com.google.appengine.api.datastore.Entity;
+import com.google.gson.Gson;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 // Represents a video, including its ID, title, description, and captions.
 public class Video {
@@ -6,12 +11,30 @@ public class Video {
     private String title;
     private String description;
     private String captions;
-
+    
     public Video(String videoId, String title, String description, String captions) {
         this.videoId = videoId;
         this.title = title;
         this.description = description;
         this.captions = captions;
+    }
+
+    public static Video httpRequestToVideo(HttpServletRequest request) {
+        // Get the input from the form.
+        String videoId = request.getParameter(PropertyNames.VIDEO_ID);
+        // Extra fields in form for metadata until we can fetch them.
+        String title = request.getParameter(PropertyNames.TITLE);
+        String description = request.getParameter(PropertyNames.DESCRIPTION);
+        String captions = request.getParameter(PropertyNames.CAPTIONS);
+        Video video = new Video(videoId, title, description, captions);
+        return video;
+    }
+
+    public static Entity videoToDatastoreEntity(Video video){
+        Entity videoEntity = new Entity("Video");
+        videoEntity.setProperty(PropertyNames.VIDEO_ID, video.getVideoId());
+        videoEntity.setProperty(PropertyNames.VIDEO_OBJECT_AS_JSON, new Gson().toJson(video));
+        return videoEntity;
     }
 
     public String getVideoId() {
--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -1,73 +1,44 @@
-
+package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
-import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
-import com.google.gson.Gson;
-import com.google.sps.data.Fields;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.sps.data.PropertyNames;
 import com.google.sps.data.Video;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 // Servlet for testing queries.
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        String id = request.getParameter(Fields.VIDEO_ID);
+        String id = request.getParameter(PropertyNames.VIDEO_ID);
         if(id == null){
-            response.getWriter().println("AAAAAAAAAAAAAAAAAAAAAA");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         }
-        Key idAsKey = KeyFactory.createKey("Video", id);
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         Query query = new Query("Video")
-        .setFilter(new FilterPredicate("videoId", FilterOperator.EQUAL, id));
+            .setFilter(new FilterPredicate("videoId", FilterOperator.EQUAL, id));
         PreparedQuery preparedQuery = datastore.prepare(query);
         Entity video = preparedQuery.asSingleEntity();
-        String videoObjectJson = video.getProperty("videoObjectAsJson").toString();
-        //Video videoObject = gson.fromJson(videoObjectJson, Video);
+        String videoObjectJson = video.getProperty(PropertyNames.VIDEO_OBJECT_AS_JSON).toString();
         response.setContentType("application/json");
         response.getWriter().println(videoObjectJson);
     }
-
-      @Override
+    
+    @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-       
-        Video video = requestToVideoObject(request);
-        Entity videoEntity = videoToEntity(video);
-
+        Video video = Video.httpRequestToVideo(request);
+        Entity videoEntity = Video.videoToDatastoreEntity(video);
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         datastore.put(videoEntity);
         response.sendRedirect("/?videoId=" + video.getVideoId());
-    }
-
-    public Video requestToVideoObject(HttpServletRequest request){
-        // Get the input from the form.
-        String videoId = request.getParameter(Fields.VIDEO_ID);
-        // Extra fields in form for metadata until we can fetch them.
-        String title = request.getParameter(Fields.TITLE);
-        String description = request.getParameter(Fields.DESCRIPTION);
-        String captions = request.getParameter(Fields.CAPTIONS);
-        Video video = new Video(videoId, title, description, captions);
-        return video;
-    }
-
-    public Entity videoToEntity(Video video){
-        Entity videoEntity = new Entity("Video");
-        videoEntity.setProperty("videoId", video.getVideoId());
-        videoEntity.setProperty("videoObjectAsJson", new Gson().toJson(video));
-        return videoEntity;
     }
 }
