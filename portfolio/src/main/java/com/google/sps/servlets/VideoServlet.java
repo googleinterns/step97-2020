ab1c00dafa0b769c1bd47cbc8eab7d4a2cbe4622
Chet Bhateja <chetbhateja@gmail.com>
2020-07-08 00:25:59 +0000

    Change Query class name since it collides with Datastore Query class

--- portfolio/src/main/java/com/google/sps/servlets/VideoServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/VideoServlet.java
@@ -1,7 +1,7 @@
 package com.google.sps.servlets;
 
 import com.google.sps.data.PropertyNames;
-import com.google.sps.data.Query;
+import com.google.sps.data.SearchQuery;
 import com.google.sps.data.SentimentTools;
 import com.google.sps.data.Video;
 
@@ -12,11 +12,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// Takes in a video and sends back the corresponding query for that video.
+// Takes in a video and sends back the corresponding search query for that video.
 @WebServlet("/video")
 public class VideoServlet extends HttpServlet {
 
-    private static int QUERY_SIZE = 7;
+    private static int SEARCH_QUERY_SIZE = 7;
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
@@ -28,12 +28,12 @@ public class VideoServlet extends HttpServlet {
         String captions = request.getParameter(PropertyNames.CAPTIONS);
         Video dummyVideo = new Video(videoId, title, description, captions);
         
-        // Calculate the query and sentiment.
-        Query query = new Query(dummyVideo, QUERY_SIZE);
+        // Calculate the search query and sentiment.
+        SearchQuery searchQuery = new SearchQuery(dummyVideo, SEARCH_QUERY_SIZE);
         float sentimentScore = new SentimentTools(dummyVideo).getScore();
 
         // Make the param string and redirect the user to the results page.
-        String paramString = "?q=" + query + "&score=" + sentimentScore + "&title=" + title;
+        String paramString = "?q=" + searchQuery + "&score=" + sentimentScore + "&title=" + title;
         response.sendRedirect("results.html" + paramString);
     }
 }
