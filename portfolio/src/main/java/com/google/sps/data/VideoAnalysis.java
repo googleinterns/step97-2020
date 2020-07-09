921806eb9ff0227d6c5f4eb3eb919140c3c34d65
QuinnDaCollins <QuinnDaCollins@gmail.com>
2020-07-09 18:02:54 -0500

    Removed setters and made variables final

--- portfolio/src/main/java/com/google/sps/data/VideoAnalysis.java
+++ portfolio/src/main/java/com/google/sps/data/VideoAnalysis.java
@@ -5,9 +5,9 @@ import com.google.sps.data.Video;
 import java.io.IOException;
 // Represents a video analysis, holds the results from an analysis and can also analyze a video that is passed in.
 public class VideoAnalysis {
-    private float sentimentScore;
-    private float sentimentMagnitude;
-    private String query;
+    private final float sentimentScore;
+    private final float sentimentMagnitude;
+    private final String query;
     private static int QUERY_SIZE = 7;
     
     //If given a video we will run analysis on it !!THIS IS SLOWER THAN JUST PASSING IN THE VALUES IF YOU HAVE THEM!!
@@ -26,24 +26,12 @@ public class VideoAnalysis {
         this.query = _query;
     }
 
-    public void setSentimentScore(float score){
-        this.sentimentScore = score;
-    }
-
     public float getSentimentScore(){
         return this.sentimentScore;
     }
 
-    public void setSentimentMagnitude(float magnitude) {
-        this.sentimentMagnitude = magnitude;
-    }
-
     public float getSentimentMagnitude() {
         return this.sentimentMagnitude;
-    }
-
-    public void setQuery(String query){
-        this.query = query;
     }
 
     public String getQuery(){
