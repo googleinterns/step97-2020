e34c25284d3d14539a7eae00944cb6ca99b65c4f
QuinnDaCollins <QuinnDaCollins@gmail.com>
2020-07-16 13:37:12 -0500

    Remove lingering close paren

--- portfolio/src/main/java/com/google/sps/data/VideoAnalysis.java
+++ portfolio/src/main/java/com/google/sps/data/VideoAnalysis.java
@@ -54,6 +54,6 @@ public class VideoAnalysis {
     public boolean isExpiredAnalysis(){
         LocalDate now = LocalDate.now();
         Period diff = Period.between(this.timestamp, now);
-        return diff.days() >= 7);
+        return (diff.days() >= 7);
     }
 }
