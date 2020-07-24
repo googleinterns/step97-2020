1d0c259fe90a57dcb321823300d1e37bc5029b00
QuinnDaCollins <QuinnDaCollins@gmail.com>
2020-07-24 16:18:12 -0500

    implement emulated datastore into dataservlet unit tests so we dont collide with realtime datastore

--- portfolio/src/test/java/DataServletUnitTest.java
+++ portfolio/src/test/java/DataServletUnitTest.java
@@ -1,4 +1,6 @@
 import com.google.sps.servlets.DataServlet;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import java.io.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +23,19 @@ import static org.mockito.Mockito.when;
 * Test[rest of name].java -> TestBlahBlahBlah.java
 */
 public class DataServletUnitTest extends Mockito {
+  private final LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig()
+  .setDefaultHighRepJobPolicyUnappliedJobPercentage(100));
+
+    @Before
+    public void setUp() {
+        helper.setUp();
+    }
+
+    @After
+    public void tearDown() {
+        helper.tearDown();
+    }
+
     @Test
     public void testDoGetWithNoVideoId() throws Exception {
         HttpServletRequest request = mock(HttpServletRequest.class);       
@@ -34,5 +49,19 @@ public class DataServletUnitTest extends Mockito {
         new DataServlet().doGet(request, response);
         writer.flush(); // it may not have been flushed yet...
         assertEquals(true, stringWriter.toString().contains("Video id cannot be empty."));
+    }
+    @Test
+    public void testDoGetWithUnusedVideoId() throws Exception {
+        HttpServletRequest request = mock(HttpServletRequest.class);       
+        HttpServletResponse response = mock(HttpServletResponse.class);    
+        //Add an empty video id parameter to the get request
+        when(request.getParameter("videoId")).thenReturn("abcdefghijklmnop");
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
+        //begin a get request with our parameter
+        new DataServlet().doGet(request, response);
+        writer.flush(); // it may not have been flushed yet...
+        assertEquals(true, stringWriter.toString().contains("Video not found in database."));
     }
 }
