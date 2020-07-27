1bcb55d6d692fe2cf37427f1cb67881ee83a5749
QuinnDaCollins <QuinnDaCollins@gmail.com>
2020-07-27 15:50:15 -0500

    Added error constants file to dataservlet unit tests file:

---
+++ portfolio/src/main/java/com/google/sps/data/ErrorConsts.java
@@ -0,0 +1,11 @@
+package com.google.sps.data;
+
+// Stores error messages as constants.
+// Prevents errors due to typos when writing a property name string.
+public class ErrorConsts {
+    public static String EMPTY_VIDEO_ID = "Video id cannot be empty.";
+    public static String VIDEO_NOT_FOUND_IN_DB = "Video not found in database.";
+    public static String UNABLE_TO_RETRIEVE = "Unable to retrieve video.";
+    public static String NO_VIDEO_QUERY_MATCHES = "No such video found.";
+    public static String SECURITY_EXCEPTION = "A security exception occurred.";
+}
