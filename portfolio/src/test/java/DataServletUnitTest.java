383d8cdaa1e0ebcdc32fd4194d6019b74ece989b
QuinnDaCollins <QuinnDaCollins@gmail.com>
2020-07-28 14:58:55 -0500

    Add success test for data servlet unit tests, add optional constructor in servlet

--- portfolio/src/test/java/DataServletUnitTest.java
+++ portfolio/src/test/java/DataServletUnitTest.java
@@ -1,4 +1,5 @@
 import com.google.sps.data.ErrorConsts;
+import com.google.sps.data.Video;
 import com.google.sps.servlets.DataServlet;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
@@ -11,6 +12,7 @@ import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.gson.Gson;
 import java.io.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -49,30 +51,51 @@ public class DataServletUnitTest extends Mockito {
 
     @Test
     public void testDoGetWithNoVideoId() throws Exception {
-        HttpServletRequest request = mock(HttpServletRequest.class);       
-        HttpServletResponse response = mock(HttpServletResponse.class);    
-        //Add an empty video id parameter to the get request
-        when(request.getParameter("videoId")).thenReturn("");
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter writer = new PrintWriter(stringWriter);
-        when(response.getWriter()).thenReturn(writer);
-        //begin a get request with our parameter
-        new DataServlet().doGet(request, response);
-        writer.flush(); // it may not have been flushed yet...
-        assertEquals(true, stringWriter.toString().contains(ErrorConsts.EMPTY_VIDEO_ID));
+      HttpServletRequest request = mock(HttpServletRequest.class);       
+      HttpServletResponse response = mock(HttpServletResponse.class);    
+      //Add an empty video id parameter to the get request
+      when(request.getParameter("videoId")).thenReturn("");
+      StringWriter stringWriter = new StringWriter();
+      PrintWriter writer = new PrintWriter(stringWriter);
+      when(response.getWriter()).thenReturn(writer);
+      //begin a get request with our parameter
+      new DataServlet().doGet(request, response);
+      writer.flush(); // it may not have been flushed yet...
+      assertEquals(true, stringWriter.toString().contains(ErrorConsts.EMPTY_VIDEO_ID));
     }
     @Test
     public void testDoGetWithUnusedVideoId() throws Exception {
-        HttpServletRequest request = mock(HttpServletRequest.class);       
-        HttpServletResponse response = mock(HttpServletResponse.class);    
-        //Add a video id parameter to the get request
-        when(request.getParameter("videoId")).thenReturn(this.TEST_VIDEO_ID);
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter writer = new PrintWriter(stringWriter);
-        when(response.getWriter()).thenReturn(writer);
-        //begin a get request with our parameter
-        new DataServlet().doGet(request, response);
-        writer.flush(); // it may not have been flushed yet...
-        assertEquals(true, stringWriter.toString().contains(ErrorConsts.VIDEO_NOT_FOUND_IN_DB));
+      HttpServletRequest request = mock(HttpServletRequest.class);       
+      HttpServletResponse response = mock(HttpServletResponse.class);    
+      //Add a video id parameter to the get request
+      when(request.getParameter("videoId")).thenReturn(this.TEST_VIDEO_ID);
+      StringWriter stringWriter = new StringWriter();
+      PrintWriter writer = new PrintWriter(stringWriter);
+      when(response.getWriter()).thenReturn(writer);
+      //begin a get request with our parameter
+      new DataServlet().doGet(request, response);
+      writer.flush(); // it may not have been flushed yet...
+      assertEquals(true, stringWriter.toString().contains(ErrorConsts.VIDEO_NOT_FOUND_IN_DB));
     }
-}
+    @Test
+    public void testDoGetWithValidId() throws Exception {
+      HttpServletRequest request = mock(HttpServletRequest.class);       
+      HttpServletResponse response = mock(HttpServletResponse.class);   
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      //create an empty video object with only id, publicity boolean, and a URL to avoid exceptions.
+      Video dummyVideo = new Video(this.TEST_VIDEO_ID, "", "", "https://www.google.com", true); 
+      Entity dummyVideoEntity = Video.videoToDatastoreEntity(dummyVideo);
+      //Put the empty video object into datastore with the custom video key
+      datastore.put(dummyVideoEntity);
+      //Add an valid video id parameter to the get request
+      when(request.getParameter("videoId")).thenReturn(this.TEST_VIDEO_ID);
+      StringWriter stringWriter = new StringWriter();
+      PrintWriter writer = new PrintWriter(stringWriter);
+      when(response.getWriter()).thenReturn(writer);
+      //begin a get request with our parameter and our own datastore instance
+      new DataServlet(datastore).doGet(request, response);
+      writer.flush(); // it may not have been flushed yet...
+      String videoObjectJsonString = new Gson().toJson(dummyVideo);   
+      assertEquals(true, stringWriter.toString().contains(videoObjectJsonString));
+    }
+  }
