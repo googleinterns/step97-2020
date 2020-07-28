434327e3a095dc25500d72eeaaf3a395726b94ec
Chet Bhateja <chetbhateja@gmail.com>
2020-07-28 22:21:29 +0000

    Add request functions and UI for analysis

---
+++ portfolio/src/main/webapp/prototypeElements.js
@@ -0,0 +1,15 @@
+// HTML element names for the prototype UI.
+elements = {
+    idField: "id-field",
+    searchForm: "search-form",
+    selectVideo: "select-video",
+    selectPlaylist: "select-playlist",
+    analyzeButton: "analyze-button",
+    analysisSection: "analysis-section",
+    listPane: "list-pane",
+    playlistEntries: "playlist-entries",
+    analysisPane: "analysis-pane",
+    analysisIframe: "analysis-iframe",
+    happyMeter: "happy-meter",
+    analysisSearch: "analysis-search"
+}
