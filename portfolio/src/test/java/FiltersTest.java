827c02dd680a376547fd13b68ca69b6621c1395d
Chet Bhateja <chetbhateja@gmail.com>
2020-07-29 00:35:47 +0000

    Address more pull request suggestions

--- portfolio/src/test/java/FiltersTest.java
+++ portfolio/src/test/java/FiltersTest.java
@@ -16,7 +16,7 @@ import org.junit.runners.JUnit4;
 
 public class FiltersTest {
     @Test
-    public void GeneralFilterTest1() throws MalformedURLException{
+    public void GeneralFilterTest() throws MalformedURLException{
         String mockTitle = "MOCK_TITLE https:// hello hi how$ a4e/you";
         String mockDescription = "MOCK_DESCRIPTION this-% is A  \n^description";
         List<String> keywordsList = Arrays.asList((mockTitle + " " + mockDescription).toLowerCase().split("\\s+"));
@@ -30,20 +30,16 @@ public class FiltersTest {
         // Filtering again without a size limit shouldn't change the query.
         String filteredQuery = SearchQuery.filterKeywords(Arrays.asList(query.split("\\s+")), 1000);
         assertEquals(query, filteredQuery);
-    }
-
-    @Test
-    public void GeneralFilterTest2() throws MalformedURLException{
-        String mockTitle = "MOCK TITLE test/test$ test hello.";
-        String mockDescription = "MOCK DESCRIPTION $$$ c4$h m0nEy/";
-        List<String> keywordsList = Arrays.asList((mockTitle + mockDescription).toLowerCase().split("\\s+"));
-        String query = SearchQuery.filterKeywords(keywordsList, 7);
+        // Try with another title and description.
+        mockTitle = "MOCK TITLE test/test$ test hello.";
+        mockDescription = "MOCK DESCRIPTION $$$ c4$h m0nEy/";
+        keywordsList = Arrays.asList((mockTitle + mockDescription).toLowerCase().split("\\s+"));
+        query = SearchQuery.filterKeywords(keywordsList, 7);
         // No special characters.
-        Pattern pattern = Pattern.compile("[^a-zA-Z0-9 ]");
-        Matcher matcher = pattern.matcher(query);
+        matcher = pattern.matcher(query);
         assertEquals(matcher.find(), false);
         // Filtering again without a size limit shouldn't change the query.
-        String filteredQuery = SearchQuery.filterKeywords(Arrays.asList(query.split("\\s+")), 1000);
+        filteredQuery = SearchQuery.filterKeywords(Arrays.asList(query.split("\\s+")), 1000);
         assertEquals(query, filteredQuery);
     }
 
@@ -65,5 +61,36 @@ public class FiltersTest {
         String query = SearchQuery.filterKeywords(keywordsList, 7);
         // Duplicates should be removed.
         assertEquals(query, "mock");
+    }
+
+    @Test
+    public void EmptyTitle() {
+        String mockTitle = "";
+        String mockDescription = "mock description te$t";
+        List<String> keywordsList = Arrays.asList((mockTitle + " " + mockDescription).toLowerCase().split("\\s+"));
+        String query = SearchQuery.filterKeywords(keywordsList, 7);
+        // Query should be from description only.
+        assertEquals(query, "mock description");
+    }
+
+    @Test
+    public void EmptyDescription() {
+        String mockTitle = "mock title h$llo";
+        String mockDescription = "";
+        List<String> keywordsList = Arrays.asList((mockTitle + " " + mockDescription).toLowerCase().split("\\s+"));
+        String query = SearchQuery.filterKeywords(keywordsList, 7);
+        // Query should be from title only.
+        assertEquals(query, "mock title");
+    }
+
+    @Test
+    public void AllEmpty() {
+        String mockTitle = "";
+        String mockDescription = "";
+        // This yields an empty keywords list.
+        List<String> keywordsList = Arrays.asList((mockTitle + " " + mockDescription).toLowerCase().split("\\s+"));
+        String query = SearchQuery.filterKeywords(keywordsList, 7);
+        // Query should be empty.
+        assertEquals(query, "");
     }
 }
