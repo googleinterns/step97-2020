e09af61c8a7b37b81104d37b23ae85281beb876d
Chet Bhateja <chetbhateja@gmail.com>
2020-07-31 02:15:48 +0000

    Replace old frontend files

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,191 +12,214 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function TestVideoObject(){
-    fetch("VideoTesting").then(response => response.text()).then((testValue) => {
-        console.log(testValue);
-    })
-}
+// The ID of the object currently being analyzed.
+let objectId = null;
+// Is the object a playlist?
+let isPlaylist = false;
+// List of videos for current playlist.
+let playlistVideos = null;
 
-//CONSTS
-const FormIDs = {
-title: "video-title",
-description: "video-description"
-}
+// Listener for submitting an object (video or playlist).
+function formListener(e) {
+    submitObject();
+    e.preventDefault();
+};
 
-//The id for the most recently loaded video.
-let videoId = null;
+document.getElementById(elements.searchForm).addEventListener('submit', formListener, false);
 
-//Most recently loaded playlist.
-let playlistId = null;
-let playlist = null;
-
-//Playlist analysis and index within it.
-let playlistAnalyses = null;
-let analysisIndex = null;
-
-//Submit video data to the data servlet.
-async function submitVideoData() {
-    //Hide the old analysis.
-    document.getElementById(elements.analysisContainer).style.display = "none";
-    document.getElementById(elements.happyMeter).style.display="none";
-    document.getElementById(elements.searchFlexbox).style.display="none";
-    document.getElementById(elements.playlistControls).style.display="none";
-
-    //Send post request with form data.
-    videoId = document.getElementById(elements.videoId).value;
-    const videoForm = document.getElementById(elements.videoDataForm);
-    const queryString = new URLSearchParams(new FormData(videoForm)).toString();
-    const request = new Request("/data?" + queryString, {method: "POST"});
+// Submit video so that data can be stored by DataServlet.
+// Returns true if sucessful.
+async function submitVideo() {
+    clearAnalysis();
+    // Get the id value from the id field.
+    let curId = document.getElementById(elements.idField).value;
+    // Request to store the video in the database.
+    const request = new Request("/data?videoId=" + curId, {method: "POST"});
     const response = await fetch(request);
     const responseText = await response.text()
-    //Alert the user if any errors occurred.
+    // Alert the user if any errors occurred.
     if (response.status >= 400) {
         alert(responseText);
-        return;
+        return false;
     }
-    await fetchVideoData(videoId);
-    document.getElementById(elements.analyzeButton).style.display = "inline-block";
+    // If request is successful, set global id variable.
+    objectId = curId;
+    isPlaylist = false;
+    playlistVideos = null;
+    return true;
 }
 
-//This function is a GET request to our database to populate our mainpage elemetns with video information
-async function fetchVideoData(videoId) {
-    const response = await fetch('/data?videoId=' + videoId);
-    //Alert the user and exit if errors occurred.
-    if (response.status >= 400) {
-        alert(await response.text());
-        return;
-    }
-    //Otherwise, update the page with the video data.
-    const videoJson = await response.json();
-    document.getElementById(FormIDs.title).innerText = videoJson.title;
-    document.getElementById(FormIDs.description).innerText = videoJson.description;
-}
-
-async function analyze() {
-    //Check if video id is properly initialized.
-    if (videoId === null) {
-        alert("No video selected.");
-        return;
-    }
-    //Post the video for analysis.
-    let request = new Request("/analysis?videoId=" + videoId, {method: "POST"});
-    let response = await fetch(request);
-    if (response.status >= 400) {
-        alert(await response.text());
-        return;
-    }
-    //Get the results of the analysis.
-    request = new Request("/analysis?videoId=" + videoId, {method: "GET"});
-    response = await fetch(request);
-    if (response.status >= 400) {
-        alert(await response.text());
-        return;
-    }
-
-    //Update and show the analysis with the response fields.
-    response = await response.json();
-    updateAnalysis(response);
-    document.getElementById(elements.analysisContainer).style.display = "block";
-    document.getElementById(elements.happyMeter).style.display="inline";
-    document.getElementById(elements.searchFlexbox).style.display="flex";
-}
-
-async function loadPlaylist() {
-    // Hide the old analysis.
-    document.getElementById(elements.titlesContainer).innerHTML = "";
-    document.getElementById(elements.analysisContainer).style.display = "none";
-    document.getElementById(elements.happyMeter).style.display="none";
-    document.getElementById(elements.searchFlexbox).style.display="none";
-    document.getElementById(elements.playlistControls).style.display="none";
-
-    // Make a post request to store playlist videos.
-    playlistId = document.getElementById(elements.playlistId).value;
-    const responseJSON = await fetch("/playlist?playlistId=" + playlistId, {method: "POST"});
-    const response = await responseJSON.json();
+// Submit playlist so that data can be stored by PlaylistServlet.
+// Returns true if successful.
+async function submitPlaylist() {
+    clearAnalysis();
+    // Make a request for the PlaylistServlet to store the playlist videos.
+    curId = document.getElementById(elements.idField).value;
+    const response = await fetch("/playlist?playlistId=" + curId, {method: "POST"});
     // Check if any errors occurred.
     if (response.status >= 400) {
         alert(await response.text());
-        return;
+        return false;
     }
+    // If no errors occurred, set global object fields.
+    objectId = curId;
+    isPlaylist = true;
+    playlistVideos = await response.json();
+    return true;
+}
 
-    // List titles of videos in the playlist.
-    const titleContainer = document.getElementById(elements.titlesContainer);
-    let count = 0;
-    for (var video of response) {
-        titleContainer.innerHTML += "<li>" + video.title + "</li>";
-        if (++count == 5) {
-            break;
-        }
+// Submit an object to be stored (either a playlist or video).
+async function submitObject() {
+    // Show loader icon
+    document.getElementById(elements.submitLoader).style.display = "";
+    // Check if the object is a playlist and run the appropriate function.
+    isPlaylist = document.getElementById(elements.isPlaylist).checked;
+    successful = isPlaylist ? await submitPlaylist() : await submitVideo();
+    // Hide loader icon
+    document.getElementById(elements.submitLoader).style.display = "none";
+    // If the load succeeded, display the analysis button.
+    if (successful) {
+        //ADD SOME PREVIEW HERE
+        document.getElementById(elements.analyzeButton).style.display = "block";
     }
-    if (response.length > 5) {
-        titleContainer.innerHTML += "<li>...</li>";
+}
+
+// Analyze the currently selected video object.
+// Returns true if successful.
+async function analyzeVideo() {
+    if (isPlaylist) {
+        alert("The selected item is not a video.");
+        return false;
     }
-
-    // Show analysis button.
-    document.getElementById(elements.analyzePlaylistButton).style.display = "inline-block";
-
-    // Update global playlist variables.
-    playlist = response;
-    playlistAnalyses = null;
-    analysisIndex = null;
+    // Ensure that global id variable is set.
+    if (objectId === null) {
+        alert("No video selected.");
+        return false;
+    }
+    // Post the video for analysis.
+    let request = new Request("/analysis?videoId=" + objectId, {method: "POST"});
+    let response = await fetch(request);
+    if (response.status >= 400) {
+        alert(await response.text());
+        return false;
+    }
+    // Get the results of the analysis.
+    request = new Request("/analysis?videoId=" + objectId, {method: "GET"});
+    response = await fetch(request);
+    // Check for errors
+    if (response.status >= 400) {
+        alert(await response.text());
+        return false;
+    }
+    // Update and show the analysis with the response fields.
+    videoAnalysis = await response.json();
+    showVideoAnalysis(videoAnalysis);
+    // Don't need to display list pane for video analysis.
+    document.getElementById(elements.listPane).style.display = "none";
+    return true;
 }
 
 async function analyzePlaylist() {
+    if (!isPlaylist) {
+        alert("The selected item is not a playlist.");
+        return false;
+    }
+    // Ensure that global id variable is set.
+    if (objectId === null) {
+        alert("No playlist selected.");
+        return false;
+    }
     // Make a post request to store video analyses.
-    const requests = playlist.map(video => fetch("/analysis?videoId=" + video.videoId, {method: "POST"}));
+    const requests = playlistVideos.map(video => fetch("/analysis?videoId=" + video.videoId, {method: "POST"}));
     const responses = await Promise.all(requests);
     // Fetch the analysis results.
-    const analysisRequests = playlist.map(video => fetch("analysis?videoId=" + video.videoId, {method: "GET"}));
+    const analysisRequests = playlistVideos.map(video => fetch("analysis?videoId=" + video.videoId, {method: "GET"}));
     const analysisResponses = await Promise.all(analysisRequests);
-    // Update global variables so that the user scroll through analyses.
+    // Save analyses in global variable so that the user can scroll through them.
     playlistAnalyses = await Promise.all(analysisResponses.map(response => response.json()));
-    analysisIndex = 0;
-
-    // Update page elements with analysis results.
-    const curAnalysis = playlistAnalyses[analysisIndex];
-    updateAnalysis(curAnalysis);
-
-    // Display the analysis results.
-    document.getElementById(elements.analysisContainer).style.display = "block";
-    document.getElementById(elements.happyMeter).style.display="inline";
-    document.getElementById(elements.searchFlexbox).style.display="flex";
-    document.getElementById(elements.playlistControls).style.display="flex";
+    // Show analyses.
+    showListPane(playlistAnalyses);
+    showVideoAnalysis(playlistAnalyses[0]);
+    // Show list pane for playlist entries.
+    document.getElementById(elements.listPane).style.display = "block";
+    return true;
 }
 
-function nextVideo() {
-    // Check if there is a next video.
-    if (analysisIndex == null) {
-        alert("No playlist selected.");
-        return;
+// Analyze the selected object (either a video or playlist).
+async function analyzeObject() {
+    // Show loader icon
+    document.getElementById(elements.analyzeLoader).style.display = "";
+    // Run the appropriate function and store whether it was successful.
+    const successful = isPlaylist ? await analyzePlaylist() : await analyzeVideo();
+    // Hide loader icon
+    document.getElementById(elements.analyzeLoader).style.display = "none";
+    // If successful, jump to analysis.
+    if (successful) {
+        document.getElementById(elements.analysisSection).style.display = "flex";
+        window.location.hash="";
+        window.location.hash = "analysis-section";
+        document.getElementById(elements.idField).value = "";
+        document.getElementById(elements.analyzeButton).style.display = "none";
     }
-    if (analysisIndex == playlist.length - 1) {
-        alert("No more videos.");
-        return;
-    }
-    // Increment the analysis index and update the analysis page elements.
-    const curAnalysis = playlistAnalyses[++analysisIndex];
-    updateAnalysis(curAnalysis);
 }
 
-function prevVideo() {
-    // Make sure that there is a previous video.
-    if (analysisIndex == null) {
-        alert("No playlist selected.");
-        return;
+// Show the list pane given the analyses for a playlist.
+function showListPane(playlistAnalyses) {
+    // Add playlist entries to list pane.
+    let entryCount = 0;
+    for (let analysis of playlistAnalyses) {
+        document.getElementById(elements.playlistEntries).innerHTML += `
+        <li class="video-entry" id="entry-` + entryCount +`">
+            <p>` + analysis.videoTitle + `</p>
+            <img id="entry-thumbnail-` + entryCount + `" src="` + analysis.thumbnailUrl + `">
+        </li>`;
+        entryCount ++;
     }
-    if (analysisIndex == 0) {
-        alert("No previous videos.");
-        return;
+    // Add onclick functions and thumbnails for entries.
+    // Had to do this in a separate loop so that the onclick function would be saved.
+    entryCount = 0;
+    for (let analysis of playlistAnalyses) {
+        document.getElementById("entry-" + entryCount).onclick = function() {
+            showVideoAnalysis(analysis);
+        };
+        document.getElementById("entry-thumbnail-" + entryCount).style.width = "18vw";
+        entryCount ++;
     }
-    // Decrement the analysis index and update analysis page elements.
-    const curAnalysis = playlistAnalyses[--analysisIndex];
-    updateAnalysis(curAnalysis);
 }
 
-// Update the analysis container with a new video analysis.
-function updateAnalysis(curAnalysis) {
-    document.getElementById(elements.analysisTitle).textContent = curAnalysis.videoTitle;
-    document.getElementById(elements.happyMeter).value = curAnalysis.sentimentScore;
-    google.search.cse.element.getElement(elements.analysisSearch).execute(curAnalysis.searchQueryString);
+// Take a VideoAnalysis object and display the results in the analysis pane.
+function showVideoAnalysis(videoAnalysis) {
+    document.getElementById(elements.analysisTitle).textContent = videoAnalysis.videoTitle;
+    document.getElementById(elements.analysisIframe).src = 
+        "https://www.youtube.com/embed/" + videoAnalysis.videoId;
+    document.getElementById(elements.happyMeter).value = videoAnalysis.sentimentScore;
+    google.search.cse.element.getElement(elements.analysisSearch).execute(videoAnalysis.searchQueryString);
+}
+
+// Hides the analysis section and resets global variables.
+function clearAnalysis() {
+    objectId = null;
+    isPlaylist = null;
+    playlistVideos = null;
+    document.getElementById("playlist-entries").innerHTML = "";
+    document.getElementById(elements.analyzeButton).style.display = "none";
+    document.getElementById(elements.analysisSection).style.display = "none";
+}
+
+// Functions for a button to go top of page.
+// Based on W3Schools tutorial: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+window.onscroll = function(){updateTopButton()};
+
+function updateTopButton() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById(elements.topButton).style.display = "block";
+    } else {
+        document.getElementById(elements.topButton).style.display = "none";
+    }
+}
+
+// Go back to the top of the page for a new analysis.
+function goToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
